abd35435b1384030bcd3183ecbfe993ba886094a
CharlesABJ <117536741+CharlesABJ@users.noreply.github.com>
2023-06-20 19:37:45 +0200

    Routes Put, Get et Delete fonctionnel, soucis avec la route post, on ne peut créer qu'1 livre sinon erreur 500

--- backend/controllers/book.js
+++ backend/controllers/book.js
@@ -7,7 +7,6 @@ exports.postOneBook = (req, res, next) => {
   const bookObject = JSON.parse(req.body.book);
   delete bookObject._id;
   delete bookObject._userId;
-  console.log(req.file.name);
   const book = new Book({
     ...bookObject,
     userId: req.auth.userId,
@@ -54,54 +53,49 @@ exports.postRating = (req, res, next) => {
 };
 
 exports.putOneBook = (req, res, next) => {
-  const bookObject = req.file
-    ? {
-      ...JSON.parse(req.body.book),
-      imageUrl: `${req.protocol}://${req.get("host")}/images/${req.file.name
-        }`,
-    }
-    : { ...req.body };
+  const bookObject = req.file ? {
+    ...JSON.parse(req.body.book),
+    imageUrl: `${req.protocol}://${req.get('host')}/${req.file.name}`
+  } : { ...req.body };
+
   delete bookObject._userId;
   Book.findOne({ _id: req.params.id })
     .then((book) => {
-      if (book._userId !== req.auth.userId) {
-        return res.status(401).json({ message: "Non autorisé" });
+      if (book.userId != req.auth.userId) {
+        res.status(401).json({ message: "Vous n'êtes pas autorisé à modifier ce livre." });
+      } else {
+        Book.updateOne({ _id: req.params.id }, { ...bookObject, _id: req.params.id })
+          .then(() => res.status(200).json({ message: "Le livre a été modifié!" }))
+          .catch(error => res.status(401).json({ error }));
       }
-      Book.updateOne(
-        { _id: req.params.id },
-        { ...bookObject, _id: req.params.id }
-      )
-        .then(() => res.status(200).json({ message: "Livre mis à jour" }))
-        .catch((error) => res.status(400).json({ error }));
     })
-    .catch((error) => res.status(400).json({ error }));
+    .catch((error) => {
+      res.status(400).json({ error });
+    });
 };
 
-exports.deleteOneBook = (req, res, next) => {
 
+
+exports.deleteOneBook = (req, res, next) => {
   Book.findOne({ _id: req.params.id })
-    .then((book) => {
+    .then(book => {
       if (!book) {
         res.status(500).json({ message: "Ce livre n'existe pas." });
       }
-      if (book._userId !== req.auth.userId) {
-        return res.status(401).json({ message: "Vous n'êtes pas autorisé à supprimer ce livre." });
-      }
-
-      if (book.imageUrl) {
-        const imagePath = `./images/${book.imageUrl}`;
-        fs.unlink(imagePath, (error) => {
-          if (error) {
-            return res.status(500).json({ error });
-          }
+      if (book.userId != req.auth.userId) {
+        res.status(401).json({ message: "Vous n'êtes pas autorisé à supprimer ce livre." });
+      } else {
+        const filename = book.imageUrl.split('/images/')[1];
+        fs.unlink(`images/${filename}`, () => {
+          Book.deleteOne({ _id: req.params.id })
+            .then(() => { res.status(200).json({ message: "Le livre a été supprimé !" }) })
+            .catch(error => res.status(401).json({ error }));
         });
       }
-      Book.deleteOne({ _id: req.params.id }, { ...req.body, _id: req.params.id })
-        .then((book) => {
-          res.status(200).json(book)
-        })
-        .catch((error) => res.status(400).json({ error }));
-    }).catch((error) => res.status(400).json({ error }));
+    })
+    .catch(error => {
+      res.status(500).json({ error });
+    });
 };
 
 exports.getBestRating = (req, res, next) => {
